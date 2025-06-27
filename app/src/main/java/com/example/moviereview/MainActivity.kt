f48cb29c1c1d1dfdbdf2447ae387f059f197de05
phinhannguyen04 <phinhannguyen04@gmail.com>
2025-06-27 21:09:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/moviereview/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.moviereview
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.compose.AppTheme
+import com.example.compose.primaryLight
+import com.example.ui.theme.bodyFontFamily
+import com.example.ui.theme.displayFontFamily
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier,
+        fontFamily = bodyFontFamily,
+        color = primaryLight
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppTheme {
+        Greeting("Movie Review")
+    }
+}
